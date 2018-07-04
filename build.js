fe05edd1bde67c4ece38770bb593018b9ec8a409
RR725 <kaifei@meizu.com>
2018-07-04 16:07:50 +0800

    modify config

--- build.js
+++ build.js
@@ -1,84 +1,25 @@
-'use strict';
-var child = require('child_process');
-var fs = require('fs');
-var filedel = require('filedel');
-var reg = /(<)(version)(>)([\w.]+)(-RC)([0-9]+)(<\/\2>)/;
+'use strict'
+var child = require('child_process')
+var filedel = require('filedel')
 
-function modifyPom() {
-	fs.readFile('pom.xml', 'utf8', function opened(err, fd) {
-		var version = fd.match(reg);
-		version = version.slice(1, 8);
-		var num = version[5];
-		num = Number(num) + 1;
-		num = String(num).length < 2 ? '0' + num : num;
-		version[5] = num;
-		version = version.join('');
-		fd = fd.replace(reg, version);
-		fs.writeFile('pom.xml', fd, function (err) {
-			if (err) throw err;
-			console.log('成功修改push项目pom.xml版本号为：' + version);
-
-		});
-		fs.writeFile('../../pom.xml', fd, function (err) {
-			if (err) throw err;
-			console.log('成功修改根目录pom.xml版本号为：' + version);
-		});
-	});
-}
-
-
-
-function modify() {
-
-
-	fs.readFile('resources/push/dist/vender.js', 'utf8', function opened(err, fd) {
-		var version = '"http://push-res"+(window.location.host=="push.meizu.com"?"":".in")+".mzres.com/"';
-
-		fd = fd.replace('"http://push-res.in.mzres.com/"', version);
-		fs.writeFile('resources/push/dist/vender.js', fd, function (err) {
-			if (err) throw err;
-			console.log('兼容国际化，根据hosts不同配置不同的CDN域名');
-
-		});
-
-	});
-
-
-
-
-}
 function build(type) {
-	filedel('resources/push/dist/*').then(function () {
-		console.log('删除旧的文件');
-		console.log(type === 'cn' ? '构建国内版...，请等待' : '构建国际版...，请等待');
+  filedel('resources/push/dist/*').then(() => {
+    console.log('删除旧的文件')
+    console.log('构建中，请等待')
 
-		var query = type === 'cn' ? '' : ' --config webpack.config-in.js';
-		var proc = child.exec('webpack' + query, function (err) {
-			if (err) {
-				console.log(err);
-			} else {
-				if (type === 'in') {
-					console.log('国际版构建成功\n');
-					modify();//修改vender里写死的cdn域名，根据当前host取值
-					modifyPom();//修改pom.xml里的版本号，在原来的基础上加1
-				}
-
-				if (type === 'cn') {
-
-					console.log('国内版构建成功\n');
-					build('in');
-				}
-			}
-
-
-		});
-		proc.stdout.on('data', function (s) {
-			console.log(s.toString());
-		});
-		proc.stderr.on('data', function (s) {
-			console.log(s.toString());
-		});
-	});
-
+    var proc = child.exec('webpack --env.build', err => {
+      if (err) {
+        console.log(err)
+      } else {
+        console.log('构建成功\n')
+      }
+    })
+    proc.stdout.on('data', s => {
+      console.log(s.toString())
+    })
+    proc.stderr.on('data', s => {
+      console.log(s.toString())
+    })
+  })
 }
-build('cn');
+build('cn')
--- webpack.config.js
+++ webpack.config.js
@@ -6,78 +6,19 @@ let CommonsChunkPlugin = webpack.optimize.CommonsChunkPlugin
 
 let HtmlWebpackPlugin = require('html-webpack-plugin')
 
-let config = {
-  entry: {
-    main: './resources/push/lib/main.js',
-    push: [
-      './resources/push/lib/url-model.js',
-      './resources/push/lib/utils.js',
-      './resources/push/js/default-permission.js',
-      './resources/push/js/producttype-app.js'
-    ],
-    vender: ['match-media', 'react', 'react-dom'] //这几个抽离出来打包成vender.js
-  },
-  output: {
-    publicPath: 'http://push-res.mzres.com/',
-    chunkFilename: 'resources/push/dist/[chunkhash:8].chunk.js',
-    filename: 'resources/push/dist/[name].js?ver=<%= jsVersion%>'
-  },
-  // debug: true,
-  // devtool: 'source-map',
-  module: {
-    // noParse: [/\breact\b/],
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: [
-          {
-            loader: 'babel-loader',
-            options: {
-              presets: ['react', 'es2015'],
-
-              plugins: [
-                'transform-decorators-legacy',
-                'transform-decorators',
-                'syntax-dynamic-import',
-                'transform-class-properties',
-                'transform-runtime'
-              ]
-            }
-          }
-        ]
-      },
-      {
-        test: /\.less$/,
-        loader: 'style!css!less'
-      }, // use ! to chain loaders
-      {
-        test: /\.css$/,
-        use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: ['css-loader']
-        })
-      },
-      {
-        test: /\.(png|jpg|gif)$/,
-		loader: 'url-loader', // 用到的图片全部转base64塞到js
-		options: {
-		  limit: 8192
-		}
-      }
-    ]
-  },
-  devServer: {
-    disableHostCheck: true, //Invalid Host header错误，需要这个配置
-    proxy: {
-      '/garcia/*': {
-        target: 'http://push.meizu.com/',
-        changeOrigin: true
-      }
-    }
-  },
-  plugins: [
-    // new webpack.optimize.UglifyJsPlugin({minimize: true}),
+let config = env => {
+  let plugins = []
+  if (env.build) {
+    plugins.push(
+      new webpack.optimize.UglifyJsPlugin({
+        compress: {
+          warnings: false
+        },
+        minimize: true
+      })
+    )
+  }
+  plugins = plugins.concat([
     new ExtractTextPlugin('resources/push/dist/common.css?ver=<%= jsVersion%>'), //合并css文件
     new HtmlWebpackPlugin({
       //生成Html，自动把打包后的文件加到html中
@@ -91,7 +32,79 @@ let config = {
       //把公共的文件打包
       names: ['push', 'vender']
     })
-  ]
+  ])
+  return {
+    entry: {
+      main: './resources/push/lib/main.js',
+      push: [
+        './resources/push/lib/url-model.js',
+        './resources/push/lib/utils.js',
+        './resources/push/js/default-permission.js',
+        './resources/push/js/producttype-app.js'
+      ],
+      vender: ['match-media', 'react', 'react-dom'] //这几个抽离出来打包成vender.js
+    },
+    output: {
+      publicPath: 'http://push-res.mzres.com/',
+      chunkFilename: 'resources/push/dist/[chunkhash:8].chunk.js',
+      filename: 'resources/push/dist/[name].js?ver=<%= jsVersion%>'
+    },
+    // debug: true,
+    // devtool: 'source-map',
+    module: {
+      // noParse: [/\breact\b/],
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: [
+            {
+              loader: 'babel-loader',
+              options: {
+                presets: ['react', 'es2015', 'env'],//env用在async await
+
+                plugins: [
+                  'transform-decorators-legacy',
+                  'transform-decorators',
+                  'syntax-dynamic-import',
+                  'transform-class-properties',
+                  'transform-runtime'
+                ]
+              }
+            }
+          ]
+        },
+        {
+          test: /\.less$/,
+          loader: 'style!css!less'
+        }, // use ! to chain loaders
+        {
+          test: /\.css$/,
+          use: ExtractTextPlugin.extract({
+            fallback: 'style-loader',
+            use: ['css-loader']
+          })
+        },
+        {
+          test: /\.(png|jpg|gif)$/,
+          loader: 'url-loader', // 用到的图片全部转base64塞到js
+          options: {
+            limit: 8192
+          }
+        }
+      ]
+    },
+    devServer: {
+      disableHostCheck: true, //Invalid Host header错误，需要这个配置
+      proxy: {
+        '/garcia/*': {
+          target: 'http://push.meizu.com/',
+          changeOrigin: true
+        }
+      }
+    },
+    plugins: plugins
+  }
 }
 
 module.exports = config
