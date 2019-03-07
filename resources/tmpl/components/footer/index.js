5e975a50d42e2c33e36cdae7bc9eeab652cf1571
ecofe <ecofe@ecofedeMacBook-Air.local>
2019-03-07 20:31:53 +0800

    add beian

--- resources/tmpl/components/footer/index.js
+++ resources/tmpl/components/footer/index.js
@@ -1,21 +1,28 @@
 /*
- * @Author: ecofe 
- * @Date: 2018-07-02 09:15:24 
+ * @Author: ecofe
+ * @Date: 2018-07-02 09:15:24
  * @Last Modified by: ecofe
  * @Last Modified time: 2018-07-05 17:53:07
  */
-'use strict'
-import React from 'react'
-import './index.less'
+'use strict';
+import React from 'react';
+import './index.less';
 class Footer extends React.Component {
-  constructor(props) {
-    super(props)
-  }
-  render() {
-    const year = new Date().getFullYear()
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        const year = new Date().getFullYear();
 
-    return <div className="footer">©{year}</div>
-  }
+        return (
+            <div className="footer">
+                ©{year}{' '}
+                <a href="http://www.miitbeian.gov.cn" target="_blank">
+                    粤ICP备15059307号
+                </a>
+            </div>
+        );
+    }
 }
 
-module.exports = Footer
+module.exports = Footer;
