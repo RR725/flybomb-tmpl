3958236c9ba9a627e44e0a23df688494848e3534
ecofe <ecofe@ecofedeMacBook-Air.local>
2020-04-01 20:04:24 +0800

    修改备案

--- resources/tmpl/components/footer/index.js
+++ resources/tmpl/components/footer/index.js
@@ -4,25 +4,25 @@
  * @Last Modified by: ecofe
  * @Last Modified time: 2018-07-05 17:53:07
  */
-'use strict';
-import React from 'react';
-import './index.less';
+'use strict'
+import React from 'react'
+import './index.less'
 class Footer extends React.Component {
-    constructor(props) {
-        super(props);
-    }
-    render() {
-        const year = new Date().getFullYear();
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    const year = new Date().getFullYear()
 
-        return (
-            <div className="footer">
-                ©{year}{' '}
-                <a href="http://www.miitbeian.gov.cn" target="_blank">
-                    粤ICP备15059307号
-                </a>
-            </div>
-        );
-    }
+    return (
+      <div className="footer">
+        ©{year}{' '}
+        <a href="http://beian.miit.gov.cn/" target="_blank">
+          粤ICP备15059307号
+        </a>
+      </div>
+    )
+  }
 }
 
-module.exports = Footer;
+module.exports = Footer
